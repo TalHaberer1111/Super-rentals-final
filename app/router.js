09d8e944f6ac5a77fc2662aa0b678949aacea4c4
sara wagner and tal haberer <talhaberer2014@gmail.com>
2016-03-31 10:17:04 -0700

    add model relationship

--- app/router.js
+++ app/router.js
@@ -1,15 +1,15 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-const Router = Ember.Router.extend({
+var Router = Ember.Router.extend({
   location: config.locationType
 });
 
 Router.map(function() {
   this.route('about');
   this.route('contact');
-  this.route('announcements');
   this.route('rental', {path: '/rental/:rental_id'});
+  this.route('city', {path: '/city/:city_id'});
 });
 
 export default Router;
